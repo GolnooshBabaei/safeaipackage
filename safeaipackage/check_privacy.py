89d5225396ed0399fb21f8621a36627b1f9bf176
GolnooshBabaei <72923445+GolnooshBabaei@users.noreply.github.com>
2024-02-03 20:03:17 +0100

    Change visibility of functions

--- safeaipackage/check_privacy.py
+++ safeaipackage/check_privacy.py
@@ -1,12 +1,12 @@
 import pandas as pd
 from .util import _rga
 
-def rgs(xtrain, xtest, ytrain, ytest, model):    
+def rgp(xtrain, xtest, ytrain, ytest, model):    
     xtrain = pd.DataFrame(xtrain).reset_index(drop=True)
     xtest = pd.DataFrame(xtest).reset_index(drop=True)
     ytrain = pd.DataFrame(ytrain).reset_index(drop=True)
     ytest = pd.DataFrame(ytest).reset_index(drop=True)
-    rgs_list = []
+    rgp_list = []
     model_full = model.fit(xtrain, ytrain)
     yhat = model_full.predict(xtest)
     for i in range(len(xtrain)):
@@ -15,5 +15,5 @@ def rgs(xtrain, xtest, ytrain, ytest, model):
         ytrain_rm = ytrain.drop(index=indices[i], axis=0)
         model_rm = model.fit(xtrain_rm, ytrain_rm)
         yhat_rm = model_rm.predict(xtest)
-        rgs_list.append((1-_rga(yhat, yhat_rm)))
-    return pd.DataFrame(rgs_list, index=indices, columns=["RGS"])
+        rgp_list.append((1-_rga(yhat, yhat_rm)))
+    return pd.DataFrame(rgp_list, index=indices, columns=["RGP"])
